eb56f72f7e1f03e706fb8f833c60379e000673ce
falguni-androidster <109600225+falguni-androidster@users.noreply.github.com>
2022-09-29 07:17:51 +0530

    KOTLIN ASSIGNMENT

--- TASK/TASK 1 (Area of circle, Convert day to month to year, Hours convert into minute and second, Simple Interest  )/Areaofcircle.kt
+++
@@ -1,9 +0,0 @@
-fun main(args:Array<String>) {
-
-    var pi = 3.14
-    var radius = 10
-
-    var area = pi * radius * radius
-
-    println("Area of circle : $area")
-}
--- TASK/TASK 1 (Area of circle, Convert day to month to year, Hours convert into minute and second, Simple Interest  )/Convertmonthtoyear.kt
+++
@@ -1,8 +0,0 @@
-fun main(args:Array<String>) {
-
-    var month = 26.0
-    val year = month/12
-
-    println("Month = $month ")
-    println("Year = $year ")
-}
